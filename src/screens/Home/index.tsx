c9101a27e74692801174148048ec4eb25395e64a
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2022-03-31 16:43:40 -0300

    login e logout com twitch

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -20,6 +20,7 @@ import {
   TopGamesTitle
 } from './styles';
 import { UserFollowedStreamCard } from '../../components/UserFollowedStreamCard';
+import { Title } from '../../components/UserFollowedStreamCard/styles';
 
 interface TopGames {
   box_art_url: string, 
@@ -51,9 +52,14 @@ export function Home() {
   const { signOut, user, isLoggingOut } = useAuth();
 
   // creates a function to handle sign out
-    // try to call and wait signOut
-    // if fails, display an Alert with the title "Erro SignOut" and message "Ocorreu um erro ao tentar se deslogar do app"
-
+  async function handleSignOut(){
+    try {
+      await signOut();
+    } catch (error) {
+      Alert.alert('Erro SignOut','Ocorreu um erro ao tentar se deslogar do app');
+    }
+  }
+  
   async function getTopGames() {
     try {
       const response = await api.get('/games/top');
@@ -121,11 +127,20 @@ export function Home() {
           <UserInfoText style={{ fontFamily: theme.fonts.bold }}>{user.display_name}</UserInfoText>
         </UserInfo>
 
-        {/* <SignOutButton onPress={}>
-          Verify if isLoggingOut is true
-          If it is, show an ActivityIndicator
-          Otherwise, show Feather's power icon
-        </SignOutButton> */}
+        <SignOutButton onPress={handleSignOut}>
+          {
+            isLoggingOut ?
+              <ActivityIndicator 
+                color={theme.colors.white}
+                size={25}
+              /> :
+              <Feather 
+                name="power"
+                color={theme.colors.white}
+                size={24}
+              />
+          }
+        </SignOutButton>
       </Header>
 
       <UserFollowedStreams>
